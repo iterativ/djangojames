3922c85d290cdbee0d196f173b8ae34b831c4fc6
maersu <me@maersu.ch>
2012-06-18 21:01:32 +0200

    added sort

--- djangojames/management/commands/resetload.py
+++ djangojames/management/commands/resetload.py
@@ -19,15 +19,98 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
-
 from django.core.management.base import NoArgsCommand
 import os
 import sys
 from djangojames.db.utils import reset_schema
 from optparse import make_option
+from django.core import serializers
+from django.db.models import Model, ForeignKey, ManyToManyField, OneToOneField
+import json
+from django.db.models.loading import get_model
 
 FIXTUERS_EXT = '.json'
 
+class JsonFixtures(object):
+    
+    def __init__(self, root_dir):
+        self._root_dir = root_dir
+
+    def _extract_fixture_name(self, file_name):
+        return os.path.splitext(os.path.basename(file_name))[0]
+
+    def _get_relns(self):
+
+        def _get(model):
+            for a in models:
+                if a[0] == model:
+                    return a
+        
+        def _add(model,to):
+            to_objects = _get(to)
+            if to_objects:
+                l = relations.get(model[-1], [])
+                l.append(to_objects[-1])
+                relations[model[-1]] = l
+        
+        def _get_models(fixture_file):
+    
+            models_list = set()
+            data = json.load(open(fixture_file, 'r'))
+    
+            for o in data:
+                if o.has_key('model'):
+                    name = o['model']
+                    m = name.split('.')
+                    models_list.add((get_model(*(m[0], m[1])) , self._extract_fixture_name(fixture_file)))
+            
+            return models_list
+
+        relations = {}
+        models = []
+        for f in self.get_files():
+            models.extend(_get_models(f))
+        
+        for model in models:
+            for field in model[0]._meta.fields:
+                if any([isinstance(field, OneToOneField), 
+                        isinstance(field, ForeignKey),
+                        isinstance(field, ManyToManyField)]): 
+                    _add(model, field.rel.to)
+                        
+        return relations
+        
+    def get_files(self):
+        if not hasattr(self, '__all_fixtures'):
+            self.__all_fixtures = []
+            
+            def _find(arg, dirname, names):
+                if (dirname.endswith('fixtures')) and (dirname.find('unit_test')==-1):
+                    for name in names:
+                        if (name.endswith(FIXTUERS_EXT)) and (name.find('initial_data')==-1):
+                            self.__all_fixtures.append(os.path.join(dirname, name))                            
+            
+            os.path.walk(self._root_dir, _find, None)
+
+        return self.__all_fixtures
+    
+    def get(self):
+        return [self._extract_fixture_name(f) for f in self.get_files()]
+    
+    def get_sorted(self):
+        relns = self._get_relns()
+        
+        def _compare(a, b):    
+            l = relns.get(a, None)
+            if l and b in l:
+                return 1
+            l = relns.get(b, None)
+            if l and a in l:
+                return -1
+            return 0 
+    
+        return sorted(self.get(), _compare)
+                
 class Command(NoArgsCommand):
     option_list = NoArgsCommand.option_list + (
         make_option('-i', '--ignore_reset', action='store_true', dest='ignore_reset',
@@ -36,22 +119,8 @@ class Command(NoArgsCommand):
             help='call haystack rebuild_index command'),                                                                                                       
     )
     help = "Drops and recreates database (from jsons)."
+
     
-    def _find_fixtures(self, start_dir):
-        """ find all JSON files (except those in Unit Test Folder or initial_data) """
-        fixtures = []
-        django_fixtures = []
-        def _find(arg, dirname, names):
-            if (dirname.endswith('fixtures')) and (dirname.find('unit_test')==-1):
-                for name in names:
-                    if (name.endswith(FIXTUERS_EXT)) and (name.find('initial_data')==-1):
-                        if name.startswith('django_'):
-                            django_fixtures.append(name.replace(FIXTUERS_EXT, ''))
-                        else:
-                            fixtures.append(name.replace(FIXTUERS_EXT, ''))
-        os.path.walk(start_dir, _find, None)
-        return [django_fixtures, fixtures]
-   
     def handle_noargs(self, **options):
         from django.conf import settings
         from django.db import models
@@ -63,7 +132,7 @@ class Command(NoArgsCommand):
         rebuild_haystack = options.get('rebuild_haystack', False)
         db = options.get('database', DEFAULT_DB_ALIAS)       
         database_config = settings.DATABASES[db]
-        
+
         if not ignore_reset:
             reset_schema(database_config)
 
@@ -73,14 +142,14 @@ class Command(NoArgsCommand):
         # Emit the post sync signal. This allows individual
         # applications to respond as if the database had been
         # sync'd from scratch.
-        emit_post_sync_signal(models.get_models(), 0, 0, db)
+        emit_post_sync_signal(models.get_models(), 0, 0, db)        
         # get all fixtures
-        fixtures_blocks = self._find_fixtures(settings.PROJECT_ROOT)
-        
-        for fixtures in fixtures_blocks:
-            sys.stdout.write("Load fixtures: %s\n" % " ".join(fixtures))
-            call_command('loaddata', *fixtures)
+        jf = JsonFixtures(settings.PROJECT_ROOT)
+        fixtures = jf.get_sorted()
+
+        sys.stdout.write("Load fixtures: %s\n" % " ".join(fixtures))
+        call_command('loaddata', *fixtures)
         
         if rebuild_haystack:
             call_command('rebuild_index', interactive=False)
-
+        
