65f7dbf1ebf4db1b74f5a5c223254a6eb7fc5e6e
Daniel Egger <daniel.egger@gmail.com>
2019-09-02 17:53:04 +0200

    `from __future__ imports must come first

--- djangojames/models/export.py
+++ djangojames/models/export.py
@@ -19,8 +19,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
-from collections import OrderedDict
 from __future__ import unicode_literals
+from collections import OrderedDict
 
 from django.template.defaultfilters import slugify
 from django.http import HttpResponse
